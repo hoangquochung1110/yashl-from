f2240ef591e31172067bd39fb4e0e1e7937a361e
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-11-03 18:30:30 +0700

    Make real request to server

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -18,11 +18,20 @@ export async function shortenUrl(formData: FormData) {
     return { error: 'Invalid URL' }
   }
 
-  const shortKey = nanoid(6) // Generate a 6-character key
-  
+  const response = await fetch('https://3zrw4n9tgb.execute-api.ap-southeast-1.amazonaws.com/Dev/api', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      destination_url: url
+    }),
+  })
+
+  const data = await response.json()
+  const shortKey= JSON.parse(data.body) // Take the key from the response body
   // In a real application, we would check if the key already exists in the database
   // and generate a new one if it does
-  
   urlMap.set(shortKey, url)
 
   const shortUrl = `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/${shortKey}`
