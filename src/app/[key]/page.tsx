5b48ff381ed5b659b0e74030da5d5331ae90e11c
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-12-10 15:21:56 +0700

    Use hook to redirect destinationUrl

--- src/app/[key]/page.tsx
+++ src/app/[key]/page.tsx
@@ -1,5 +1,6 @@
-import getDestinationUrl from "@/app/lib/getDestinationUrl"
-
+"use client"
+import { useEffect, useState } from 'react';
+import getDestinationUrl from "@/app/lib/getDestinationUrl";
 
 type Params = {
   params: {
@@ -7,10 +8,31 @@ type Params = {
   }
 }
 
-export default async function ResolvePage({ params: { key } }: Params) {
-  const destinationUrlData = getDestinationUrl(key)
-  const [destinationUrl] = await Promise.all([destinationUrlData])
-  return (
-    <div>{destinationUrl.destination_url}</div>
-  )
+export default function ResolvePage({ params: { key } }: Params) {
+  const [destinationUrl, setDestinationUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchDestinationUrl = async () => {
+      try {
+        const destinationUrlData = getDestinationUrl(key);
+        const [urlData] = await Promise.all([destinationUrlData]);
+        
+        // Set the destination URL in state
+        setDestinationUrl(urlData.destination_url);
+      } catch (error) {
+        console.error("Error fetching destination URL:", error);
+      }
+    };
+
+    fetchDestinationUrl();
+  }, [key]);
+
+  useEffect(() => {
+    if (destinationUrl) {
+      // Redirect to the destination URL
+      window.location.href = destinationUrl;
+    }
+  }, [destinationUrl]);
+
+  return null; // No need to render anything as we're redirecting
 }
--- src/app/lib/getDestinationUrl.tsx
+++ src/app/lib/getDestinationUrl.tsx
@@ -1,6 +1,6 @@
-export default async function getDestinationUrl(shortUrl: string) {
-    console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/${shortUrl}`)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${shortUrl}`)
+export default async function getDestinationUrl(key: string) {
+    console.log(`${process.env.NEXT_PUBLIC_BASE_URL}/${key}`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${key}`)
 
     if (!res.ok) throw new Error('Failed to fetch destination URL')
     return res.json()
