eee843a7ba60bfb867ac3a320c82ecc14e1fa901
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2024-12-29 14:34:54 +0700

    Unify env vars

--- src/app/lib/getDestinationUrl.ts
+++ src/app/lib/getDestinationUrl.ts
@@ -1,6 +1,5 @@
 export default async function getDestinationUrl(key: string) {
-    console.log(`${process.env.NEXT_PUBLIC_RESOLVE_KEY_URL}/${key}`)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_RESOLVE_KEY_URL}/${key}`)
+    const res = await fetch(`https://${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/${key}`)
 
     if (!res.ok) throw new Error('Failed to fetch destination URL')
     return res.json()
